5c2799f6ce7fcc49a7ef3f4e59402c2e96453aa5
ghost <machuliak1337@gmail.com>
2024-12-08 00:01:27 +0100

    Added a registration button instead of the back button

--- app/src/main/java/com/example/coffeeadpp/login_activity.kt
+++ app/src/main/java/com/example/coffeeadpp/login_activity.kt
@@ -6,11 +6,13 @@ import android.widget.ImageButton
 import android.content.Intent
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.reflect.typeOf
 
 
 class login_activity : AppCompatActivity() {
@@ -27,15 +29,16 @@ class login_activity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
-        val closeBtn: ImageButton = findViewById(R.id.closeButton)
         val inputPsw: EditText = findViewById(R.id.inputPsw)
         val emailInpt: EditText = findViewById(R.id.inputE)
         val loginBtn: Button = findViewById(R.id.loginButton)
+        val registerBtn: TextView = findViewById(R.id.registerText)
 
         dbHelper = DataBaseHelper(this)
 
-        closeBtn.setOnClickListener{
-            finish()
+        registerBtn.setOnClickListener{
+            val intent = Intent(this, register_activity::class.java)
+            startActivity(intent)
         }
 
         loginBtn.setOnClickListener{
