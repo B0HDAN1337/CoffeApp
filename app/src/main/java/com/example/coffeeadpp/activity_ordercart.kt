a15e96e64c69cd3576b595e960d6a9682d0add75
ghost <machuliak1337@gmail.com>
2024-12-15 00:46:08 +0100

    Added to display on a screen what we want to buy

--- app/src/main/java/com/example/coffeeadpp/activity_ordercart.kt
+++ app/src/main/java/com/example/coffeeadpp/activity_ordercart.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.content.Intent
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -19,12 +20,27 @@ class activity_ordercart : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_ordercart)
 
-
+        val cartItems = intent.getSerializableExtra("cartItems") as ArrayList<CartItem>
+        val cartItemsLayout: LinearLayout = findViewById(R.id.cartItems)
         val backBtn: ImageButton = findViewById(R.id.backButton)
+        val totalPriceView: TextView = findViewById(R.id.totalPrice)
 
         backBtn.setOnClickListener {
             finish()
         }
+
+        var totalPrice: Double = 0.0
+
+        cartItems?.forEach { item ->
+            val itemTextView = TextView(this)
+            itemTextView.text = "${item.name} - \$${"%.2f".format(item.price)}"
+            itemTextView.textSize = 18f
+            cartItemsLayout.addView(itemTextView)
+            totalPrice += item.price
+        }
+
+        totalPriceView.text = "Total \$${"%.2f".format(totalPrice)}"
+
 
 
     }
