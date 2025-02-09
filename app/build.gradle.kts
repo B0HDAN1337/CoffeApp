bdbe9a6ba1b788b56b9d42b05f93b18461754ccb
ghost <machuliak1337@gmail.com>
2025-02-09 01:19:52 +0100

    change to android 6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
 
     defaultConfig {
         applicationId = "com.example.coffeeadpp"
-        minSdk = 24
-        targetSdk = 34
+        minSdk = 23
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
