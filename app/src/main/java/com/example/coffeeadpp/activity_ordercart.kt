c3221ccdf1186934ecbf6e732968cffc0d7437d1
ghost <machuliak1337@gmail.com>
2025-02-23 00:11:16 +0100

    adaptive for most phones in order cart

--- app/src/main/java/com/example/coffeeadpp/activity_ordercart.kt
+++ app/src/main/java/com/example/coffeeadpp/activity_ordercart.kt
@@ -6,9 +6,11 @@ import android.graphics.Typeface
 import androidx.core.content.res.ResourcesCompat
 import android.content.Intent
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
